191fecb2882dd38cac49ec4f5d255e2065d1bdb8
drianlarde <aadabelarde2020@plm.edu.ph>
2024-02-01 00:58:38 +0800

    Initial Commit

---
+++ test-page-swift/test_page_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_page_swiftApp.swift
+//  test-page-swift
+//
+//  Created by Adrian on 2/1/24.
+//
+
+import SwiftUI
+
+@main
+struct test_page_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
